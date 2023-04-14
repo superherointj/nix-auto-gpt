9f03b865aced77ea98cb963f74a9a7b947888e37
superherointj <5861043+superherointj@users.noreply.github.com>
2023-04-14 10:44:02 -0300

    removed flake-utils

--- flake.nix
+++ flake.nix
@@ -1,14 +1,20 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system}; in {
-        devShell = pkgs.callPackage ./shell.nix { };
+  outputs = { self, nixpkgs }:
+  let
+    forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+  in
+  {
+    devShells = forAllSystems (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      {
+        default = pkgs.callPackage ./shell.nix { };
       }
     );
+  };
 
 }
+
